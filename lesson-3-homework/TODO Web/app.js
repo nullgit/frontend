6d0ef5e903f279ca5adc7871c75b63e5588533aa
nullgit <2450264253@qq.com>
2021-03-24 19:04:45 +0800

    作业二

--- lesson-3-homework/TODO Web/app.js
+++ lesson-3-homework/TODO Web/app.js
@@ -34,32 +34,32 @@ button.onclick = function() {
 
     tasks.appendChild(li)
     input.value = ""
-    }
+}
 
-    clear.onclick = function() {
+clear.onclick = function() {
     tasks.innerHTML = ""
-    }
+}
 
-    function addClass(obj , cn){
-        if(!hasClass(obj , cn)){
-            obj.className += " " + cn;
-        }
+function addClass(obj , cn){
+    if(!hasClass(obj , cn)){
+        obj.className += " " + cn;
     }
+}
 
-    function hasClass(obj , cn){
-        var reg = new RegExp("\\b"+cn+"\\b");
-        return reg.test(obj.className);
-    }
+function hasClass(obj , cn){
+    var reg = new RegExp("\\b"+cn+"\\b");
+    return reg.test(obj.className);
+}
 
-    function removeClass(obj , cn){
-        var reg = new RegExp("\\b"+cn+"\\b");
-        obj.className = obj.className.replace(reg , "");
-    }
+function removeClass(obj , cn){
+    var reg = new RegExp("\\b"+cn+"\\b");
+    obj.className = obj.className.replace(reg , "");
+}
 
-    function toggleClass(obj , cn){
-        if(hasClass(obj , cn)){
+function toggleClass(obj , cn) {
+    if(hasClass(obj , cn)){
         removeClass(obj , cn);
-        }else{
+    }else{
         addClass(obj , cn);
     }
 }
